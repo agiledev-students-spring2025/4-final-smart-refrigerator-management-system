3299bfcf1414df1c7f022e30a9727ddb8ea7040a
SajidRahman310 <sr6068@nyu.edu>
2025-03-12 00:58:41 +0700

    Shopping rec page implementation

--- front-end/src/components/ShoppingRecommendation.js
+++ front-end/src/components/ShoppingRecommendation.js
@@ -1,7 +1,29 @@
 import React from "react";
 import { useInventory } from "../contexts/InventoryContext";
-import "./styles.css";
+import "./Analytics.css";
 
-const ShoppingRecommendation = () => {};
+const ShoppingRecommendation = () => {
+    const { getItemsByCompartment } = useInventory();
+    const compartments = getItemsByCompartment();
+
+    const lowStockItems = [];
+    Object.values(compartments).forEach((items) => {
+        items.forEach((item) => {
+            if (item.quantity && item.quantity <= 2) {
+                lowStockItems.push(item);
+            }
+        });
+    });
+
+    return (
+        <div className="container">
+            <h2>Shopping Recommendation</h2>
+            <p><strong>Low Stock Items:</strong> {lowStockItems.length}</p>
+            <ul>
+                {lowStockItems.map((item) => <li key={item.id}>{item.name} (Qty: {item.quantity})</li>)}
+            </ul>
+        </div>
+    );
+};
 
 export default ShoppingRecommendation;
