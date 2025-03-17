1ff86ead53bfd1918ec80f29796e178e8f08cc07
SherryKu <kexunliu02@gmail.com>
2025-03-17 01:42:56 -0400

    Updated local branch with latest master branch

--- front-end/src/components/SettingProfile.js
+++ front-end/src/components/SettingProfile.js
@@ -14,16 +14,19 @@ function SettingProfile() {
             <div className="header">
                 <h1>Account Settings</h1>
                 <div className= "sub-header">
-                    <h2>John White</h2>
-                    <h2>johnwhite.example.com</h2>
+                    <img src="https://picsum.photos/200" alt="profile" class="profile"/>
+                    <div className="header-h2">
+                        <p>John White</p>
+                        <p>johnwhite.example.com</p>
+                    </div>
                 </div>
             </div>
             <div className= "settings-option">
-                <button className="option">Account Settings</button>
+                <button onClick={() =>navigate('/Account-Setting')}className="option">Account Settings</button>
                 <button onClick={() =>navigate('/Fridge-Model')} className="option">Refrigerator Model Setup</button>
                 <button className="option">Notifications</button>
                 <button onClick={() =>navigate('/DietaryPrefernece')} className="option">Dietary Preferences</button>
-                <button className="option">Help & Support</button>
+                <button onClick={() =>navigate('/Help-Support')}className="option">Help & Support</button>
                 <button onClick={handleLogout} className="option">Logout</button>
             </div>
         </div>
