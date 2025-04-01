75ad19be955e2c506c79a2be0e74fc0c1555cedd
verozhao <zz2906@nyu.edu>
2025-04-01 07:55:59 -0400

    update back-end/mockData/items.js

---
+++ back-end/mockData/items.js
@@ -0,0 +1,85 @@
+// src/mockData/items.js
+const mockItems = [
+    {
+      id: '1',
+      name: 'Milk',
+      category: 'Dairy',
+      expirationDate: '2025-04-10',
+      quantity: '1 gallon',
+      storageLocation: 'Door shelf',
+      dateAdded: '2025-03-25',
+      imageUrl: 'https://picsum.photos/200/300',
+      nutritionInfo: {
+        calories: 100,
+        protein: '8g',
+        fat: '2.4g',
+        carbs: '12g'
+      }
+    },
+    {
+      id: '2',
+      name: 'Eggs',
+      category: 'Dairy',
+      expirationDate: '2025-04-15',
+      quantity: '12 count',
+      storageLocation: 'Door shelf',
+      dateAdded: '2025-03-26',
+      imageUrl: 'https://picsum.photos/200/300',
+      nutritionInfo: {
+        calories: 70,
+        protein: '6g',
+        fat: '5g',
+        carbs: '0g'
+      }
+    },
+    {
+      id: '3',
+      name: 'Spinach',
+      category: 'Vegetables',
+      expirationDate: '2025-04-05',
+      quantity: '1 bag',
+      storageLocation: 'Crisper drawer',
+      dateAdded: '2025-03-27',
+      imageUrl: 'https://picsum.photos/200/300',
+      nutritionInfo: {
+        calories: 20,
+        protein: '2g',
+        fat: '0g',
+        carbs: '3g'
+      }
+    },
+    {
+      id: '4',
+      name: 'Chicken breast',
+      category: 'Meat',
+      expirationDate: '2025-04-02',
+      quantity: '1 lb',
+      storageLocation: 'Meat drawer',
+      dateAdded: '2025-03-28',
+      imageUrl: 'https://picsum.photos/200/300',
+      nutritionInfo: {
+        calories: 120,
+        protein: '26g',
+        fat: '2g',
+        carbs: '0g'
+      }
+    },
+    {
+      id: '5',
+      name: 'Apples',
+      category: 'Fruits',
+      expirationDate: '2025-04-12',
+      quantity: '6 count',
+      storageLocation: 'Crisper drawer',
+      dateAdded: '2025-03-28',
+      imageUrl: 'https://picsum.photos/200/300',
+      nutritionInfo: {
+        calories: 95,
+        protein: '0.5g',
+        fat: '0.3g',
+        carbs: '25g'
+      }
+    }
+  ];
+  
+  module.exports = mockItems;
