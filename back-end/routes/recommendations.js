b67d069afe40ed00cc3133228f4b5837441df8af
SajidRahman310 <sr6068@nyu.edu>
2025-04-09 17:24:42 +0400

    Added recommendations route with replenish

---
+++ back-end/routes/recommendations.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const inventory = require('../mockData/items');
+
+router.get("/recommendations", (req, res) => {
+    const daysAhead = parseInt(req.query.daysAhead || 7);
+    const today = new Date();
+
+    const mustBuy = [];
+    const replenish = [];
+
+    inventory.forEach(item => {
+        const daysLeft = Math.ceil((new Date(item.expirationDate) - today) / (1000 * 60 * 60 * 24));
+        if (daysLeft >= 0 && daysLeft <= daysAhead) mustBuy.push(item.name);
+        else if (daysLeft > daysAhead && daysLeft <= daysAhead + 7) replenish.push(item.name);
+    });
+
+    res.json({ mustBuy, replenish });
+});
+
+module.exports = router;
