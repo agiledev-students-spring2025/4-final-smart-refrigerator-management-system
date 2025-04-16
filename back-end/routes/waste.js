4f0f0a504660efd83ec52448ae8cf5d87fa90d4d
SajidRahman310 <sr6068@nyu.edu>
2025-04-16 04:11:55 +0400

    waste database prep

--- back-end/routes/waste.js
+++ back-end/routes/waste.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const inventory = require('../mockData/items');
+const inventory = require('../mockData/items'); //remove later
 
 router.get("/waste", (req, res) => {
     const { startDate, endDate } = req.query;
@@ -22,3 +22,35 @@ router.get("/waste", (req, res) => {
 });
 
 module.exports = router;
+
+// const Item = require("../models/Item"); // Use teammate's model
+//
+// router.get("/", async (req, res) => {
+//     try {
+//         const { startDate, endDate } = req.query;
+//
+//         if (!startDate || !endDate) {
+//             return res.status(400).json({ error: "Start and end dates are required" });
+//         }
+//
+//         const start = new Date(startDate);
+//         const end = new Date(endDate);
+//
+//         const expiredItems = await Item.find({
+//             expirationDate: { $gte: start, $lte: end }
+//         });
+//
+//         const breakdown = {};
+//         expiredItems.forEach(item => {
+//             const c = item.storageLocation || "other";
+//             breakdown[c] = (breakdown[c] || 0) + 1;
+//         });
+//
+//         res.json({ totalExpired: expiredItems.length, breakdown });
+//     } catch (err) {
+//         console.error("Waste API error:", err);
+//         res.status(500).json({ error: "Internal server error" });
+//     }
+// });
+//
+// module.exports = router;
